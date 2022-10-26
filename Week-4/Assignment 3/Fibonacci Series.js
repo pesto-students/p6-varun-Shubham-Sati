27053ba3a9a26e80dd6fe0c37b8dbb51284569ab
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2022-10-26 10:06:01 +0530

    Add files via upload

---
+++ Week-4/Assignment 3/Fibonacci Series.js
@@ -0,0 +1,35 @@
+
+// Fibonacci Series with Iterators
+
+const Fib = (n) => ({
+    [Symbol.iterator]: () => {
+        let i = 0;
+        let a = 0;
+        let b = 0; 
+        return { 
+            next : () => {
+                if( i++ <  n){
+                    [a , b] = [b , (a + b) || 1];
+                    return {
+                        value: a,
+                        done: false,
+                    }
+                }else{
+                    return {
+                        value : undefined,
+                        done : true,
+                    }
+                }
+            } 
+        }
+    }
+});
+
+
+console.log( [...Fib(6)] );
+
+for( let num of Fib(6) ){
+    console.log(num);
+}
+
+
---
+++ Week-4/readme.md
@@ -0,0 +1,23 @@
+
+## Assignment 1 
+Implement a function named as getNumber() which generate a random number. If randomNumber is divisible by 5 it will reject the promise else it will resolve the promise.
+
+## Assignment 2 
+
+Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
+[teacher's name] is now teaching [subject]
+
+## Assignment 2
+
+Fibonacci Series with Iterators
+
+
+## Got to know about many topics in depth
+    1. callbacks
+    2. promises
+    3. Inheritance
+    4. Prototype
+    5. Iterators
+    6. Iterables
+
+
